301994e063affecf76f4b9bc81bf0a6424ea38a5
DIDRICH Damien <89045920+skunkoff1@users.noreply.github.com>
2022-04-04 18:16:18 +0200

    add project model

---
+++ models/Project.php
@@ -0,0 +1,19 @@
+<?php
+
+class Project {
+    private $id;
+    private $title;
+    private $headerText;
+    private $headerImg;
+    private $img1;
+    private $img2;
+    private $img3;
+    private $text1;
+    private $text2;
+    private $text3;
+
+    public function __construct() {        
+    }
+
+    
+}
